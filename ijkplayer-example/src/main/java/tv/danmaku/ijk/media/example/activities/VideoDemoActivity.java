c6704e494d37dd3621c8e8af13c1305b5778460b
Nelson <lilei@vsoontech.com>
2019-05-07 10:03:55 +0800

    提交ijkplayer 播放器，较为清洁的工程

---
+++ ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/activities/VideoDemoActivity.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2015 Bilibili
+ * Copyright (C) 2015 Zhang Rui <bbcallen@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tv.danmaku.ijk.media.example.activities;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import tv.danmaku.ijk.media.example.R;
+import tv.danmaku.ijk.media.player.Settings;
+import tv.danmaku.ijk.media.player.widget.media.AndroidMediaController;
+import tv.danmaku.ijk.media.player.widget.media.IjkVideoView;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class VideoDemoActivity extends AppCompatActivity  {
+
+    private static final String TAG = "VideoActivity";
+
+    private AndroidMediaController mMediaController;
+    private IjkVideoView mVideoView;
+    private EditText mUrlEt;
+    private Button mStartBt;
+    private Settings mSettings;
+    private boolean mBackPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        mSettings = new Settings(this);
+
+        mMediaController = new AndroidMediaController(this, false);
+
+        // init player
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mVideoView = (IjkVideoView) findViewById(R.id.video_view);
+        mVideoView.setMediaController(mMediaController);
+
+        mUrlEt = (EditText) findViewById(R.id.url_et);
+        mStartBt = (Button) findViewById(R.id.play_bt);
+    }
+
+    @Override
+    public void onBackPressed() {
+        mBackPressed = true;
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBackPressed || !mVideoView.isBackgroundPlayEnabled()) {
+            mVideoView.stopPlayback();
+            mVideoView.release(true);
+            mVideoView.stopBackgroundPlay();
+        } else {
+            mVideoView.enterBackground();
+        }
+        IjkMediaPlayer.native_profileEnd();
+    }
+
+    public void onClick(View view){
+        String url = mUrlEt.getText().toString();
+        // prefer mVideoPath
+        mVideoView.setVideoURI(Uri.parse(url));
+        mVideoView.start();
+    }
+}
