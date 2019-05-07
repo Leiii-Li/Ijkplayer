408c0eafd2e9fbf2537c1545c58ce77878d640e3
Nelson <lilei@vsoontech.com>
2019-05-07 10:45:37 +0800

    删除VideoView中当解析网络链接失败时弹窗的提示

--- ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/widget/media/IjkVideoView.java
+++ ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/widget/media/IjkVideoView.java
@@ -26,7 +26,6 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -36,7 +35,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.MediaController;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.io.File;
@@ -52,9 +50,7 @@ import tv.danmaku.ijk.media.player.IjkTimedText;
 import tv.danmaku.ijk.media.player.R;
 import tv.danmaku.ijk.media.player.TextureMediaPlayer;
 import tv.danmaku.ijk.media.player.misc.IMediaDataSource;
-import tv.danmaku.ijk.media.player.misc.IMediaFormat;
 import tv.danmaku.ijk.media.player.misc.ITrackInfo;
-import tv.danmaku.ijk.media.player.misc.IjkMediaFormat;
 import tv.danmaku.ijk.media.player.Settings;
 import tv.danmaku.ijk.media.player.MediaPlayerService;
 
@@ -540,38 +536,6 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
                     if (mOnErrorListener.onError(mMediaPlayer, framework_err, impl_err)) {
                         return true;
                     }
-                }
-
-                /* Otherwise, pop up an error dialog so the user knows that
-                 * something bad has happened. Only try and pop up the dialog
-                 * if we're attached to a window. When we're going away and no
-                 * longer have a window, don't bother showing the user an error.
-                 */
-                if (getWindowToken() != null) {
-                    Resources r = mAppContext.getResources();
-                    int messageId;
-
-                    if (framework_err == MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK) {
-                        messageId = R.string.VideoView_error_text_invalid_progressive_playback;
-                    } else {
-                        messageId = R.string.VideoView_error_text_unknown;
-                    }
-
-                    new AlertDialog.Builder(getContext())
-                        .setMessage(messageId)
-                        .setPositiveButton(R.string.VideoView_error_button,
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int whichButton) {
-                                    /* If we get here, there is no onError listener, so
-                                     * at least inform them that the video is over.
-                                     */
-                                    if (mOnCompletionListener != null) {
-                                        mOnCompletionListener.onCompletion(mMediaPlayer);
-                                    }
-                                }
-                            })
-                        .setCancelable(false)
-                        .show();
                 }
                 return true;
             }
